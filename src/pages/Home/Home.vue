d603a2e63dd82ec7aa92f0f974fba8a2d6d099d8
jloska-revolve <jakub.loska@revolve.pro>
2024-04-25 00:30:17 +0200

    Added const

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,36 +1,55 @@
 <template>
   <div style="display: flex; flex-direction: column">
-    <h2 v-if="showItems">Widać albo nie widać</h2>
+    <h2 v-if="showItems">Fruits:</h2>
+    <ul v-if="showItems">
+      <li v-for="item in items">{{ item }}</li>
+    </ul>
+    <p v-else>No fruits to display.</p>
+    <button type="button" @click="toggleShowItems">
+      Toggle Items
+    </button>
     <input v-model="userInput" />
+    <h2>{{ userInput }}</h2>
+    <div class="normal">Class Example</div>
+    <div :class="highlightClass">Class Binding Example</div>
+    <div :style="textStyle">Style Binding Example</div>
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
+    items: ["Apple", "Banana", "Orange"],
+    showItems: true,
     userInput: "",
+    isHighlighted: false,
+    textColor: "red",
+    textSize: 16,
   }),
-  props: {
-    // component recived properties
-  },
-  components: {
-    // components register
+  computed: {
+    highlightClass() {
+      if (this.showItems) {
+        return {
+          normal: true,
+        };
+      } else {
+        return {
+          highlight: true,
+        };
+      }
+    },
+    textStyle() {
+      return {
+        color: this.textColor,
+        fontSize: this.textSize + "px",
+      };
+    },
   },
   methods: {
-    // component methods
-  },
-  computed: {
-    // component computed properties
-  },
-  watch: {
-    userInput() {
+    toggleShowItems() {
       this.showItems = !this.showItems;
     },
   },
-  async created() {},
-  async mounted() {},
-  async destroyed() {},
-  async beforeMount() {},
 };
 </script>
 
