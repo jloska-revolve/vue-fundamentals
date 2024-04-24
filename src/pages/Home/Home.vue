1d3cd22446ede931e48eedb482f872e8583b291a
jloska-revolve <jakub.loska@revolve.pro>
2024-04-25 00:40:44 +0200

    props

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,49 +1,27 @@
 <template>
   <div style="display: flex; flex-direction: column">
-    <h2 v-if="showItems">Fruits:</h2>
-    <ul v-if="showItems">
-      <li v-for="item in items">{{ item }}</li>
-    </ul>
-    <p v-else>No fruits to display.</p>
+    <ListItem :showItems="showItems" :items="fruits"/>
     <button type="button" @click="toggleShowItems">
       Toggle Items
     </button>
-    <input v-model="userInput" />
-    <h2>{{ userInput }}</h2>
-    <div class="normal">Class Example</div>
-    <div :class="highlightClass">Class Binding Example</div>
-    <div :style="textStyle">Style Binding Example</div>
   </div>
 </template>
 
 <script>
+import { FRUITS } from './const';
+import ListItem from './components/ListItem.vue'
+
 export default {
   data: () => ({
-    items: ["Apple", "Banana", "Orange"],
+    fruits: FRUITS,
     showItems: true,
     userInput: "",
     isHighlighted: false,
     textColor: "red",
     textSize: 16,
   }),
-  computed: {
-    highlightClass() {
-      if (this.showItems) {
-        return {
-          normal: true,
-        };
-      } else {
-        return {
-          highlight: true,
-        };
-      }
-    },
-    textStyle() {
-      return {
-        color: this.textColor,
-        fontSize: this.textSize + "px",
-      };
-    },
+  components: {
+    ListItem
   },
   methods: {
     toggleShowItems() {
---
+++ src/pages/Home/components/ListItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div style="display: flex; flex-direction: column">
+    <h2 v-if="showItems">Fruits:</h2>
+    <ul v-if="showItems">
+      <li v-for="item in items">{{ item }}</li>
+    </ul>
+    <p v-else>No fruits to display.</p>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'ListItem',
+    props:
+    {
+       items: Array,
+       showItems: Boolean
+    }
+}
+</script>
